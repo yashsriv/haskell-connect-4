f3fce5710932ae53bceee71f77edbcf8e8120e6b
Yash Srivastav <admin@yashsriv.org>
2018-04-15 19:49:06 +0530

    Add randomness to negmax

--- src/AI.hs
+++ src/AI.hs
@@ -9,22 +9,33 @@ class GamePosition a where
   moves :: a -> [a]
   static :: a -> Int
 
-negmax :: GamePosition a => Int -> a -> (Maybe Int, Int)
+gatherMax :: [(Int, (Maybe Int, Int))] -> (Int, (Maybe Int, Int)) -> [(Int, (Maybe Int, Int))]
+gatherMax [] x = x:[]
+gatherMax xs@((_, (_, bscore)):_) x@(_, (_, score)) =
+  if score == bscore
+  then x:xs
+  else if (score < bscore)
+       then x:[]
+       else xs
+
+negmax :: (GamePosition a) => Int -> a -> IO (Maybe Int, Int)
 negmax depth b
- | depth == 0 = (Nothing, static b)
- | null (moves b) = (Nothing, static b)
- | otherwise = let results = map (negmax (depth - 1)) (moves b)
-                   zipped = zip [1..] results
-                   best = maximumBy (comparing (negate . snd . snd)) zipped
-                   (moveNum, (_, score)) = best
-                in (Just moveNum, negate score)
+ | depth == 0 = return (Nothing, static b)
+ | null (moves b) = return (Nothing, static b)
+ | otherwise = do
+     results <- sequence $ map (negmax (depth - 1)) (moves b)
+     let zipped = zip [0..] results
+         bests = foldl gatherMax [] zipped
+     best <- getStdRandom (randomPick bests)
+     let (moveIndex, (_, score)) = best
+     return (Just moveIndex, negate score)
 
 randomPick :: RandomGen g => [a] -> g -> (a, g)
 randomPick xs gen = let (index, gen') = randomR (0, (length xs) - 1) gen
                     in (xs !! index, gen')
 
-ioplayer :: (Eq a, Read a, Show a) => [a] -> IO a
-ioplayer moves =
+ioplayer :: (Eq a, Read a, Show a, GamePosition b) => [a] -> b -> IO a
+ioplayer moves b =
   do putStr "Possible Moves: "
      putStrLn $ unwords $ map show moves
      putStrLn "Enter a valid move: "
@@ -34,8 +45,14 @@ ioplayer moves =
        then return move
        else do putStrLn $ "You have entered an invalid move: " ++ (show move)
                putStrLn "Please choose one of the possible moves."
-               ioplayer moves
+               ioplayer moves b
 
-randplayer :: [a] -> IO a
-randplayer moves = do threadDelay 500000
-                      getStdRandom (randomPick moves)
+randplayer :: [a] -> b -> IO a
+randplayer moves _ = do threadDelay 500000
+                        getStdRandom (randomPick moves)
+
+negmaxplayer :: GamePosition b => Int -> [a] -> b -> IO a
+negmaxplayer depth moves b =
+  do
+    (Just index, _) <- negmax depth b
+    return $ moves !! index
